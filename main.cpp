b6ad08a357976df613818520fc64a658b6c98299
Adam <adamwberck@gmail.com>
2019-03-23 19:51:49 -0400

    Test queue added.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 #include <cstdio>
 
+#include <queue>
+
+const char*  DEFAULT_DICTIONARY = "words.txt";
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::queue<const char*> test_q;
+    test_q.push("test1");
+    std::cout << test_q.front() << std::endl;
+    test_q.pop();
     return 0;
 }
