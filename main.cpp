083fe5cad880774954ef6ca985ba110083c0a7f4
Adam <adamwberck@gmail.com>
2019-03-24 13:15:18 -0400

    Changed vector read to function in helper class.

--- main.cpp
+++ main.cpp
@@ -6,30 +6,25 @@
 #include <cstring>
 #include <malloc.h>
 #include "helper.h"
+#include <stdio.h>
 
 const char*  DEFAULT_DICTIONARY = "words.txt";
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::queue<const char*> test_q;
-    std::vector<char*> word_list;
+    std::vector<char*> word_vector;
     test_q.push("test1");
     std::cout << test_q.front() << std::endl;
     test_q.pop();
 
     FILE *file ;
-    char line[256];
     file = fopen(DEFAULT_DICTIONARY, "r");
-    fgets(line, sizeof(line), file);
-    do{
-        char *input;
-        input = static_cast<char *>(malloc(sizeof(char) * strlen(line)));
-        sprintf(input,"%s", line);
-        remove_newline_char(&input);
-        word_list.push_back(input);
-    }while(fgets(line, sizeof(line), file));//keep going unless end of file
-    for(char* word : word_list){
+    read_file_as_vector(word_vector, file);
+    for(char* word : word_vector){
         std::cout << word << std::endl;
     }
 
     return 0;
 }
+
